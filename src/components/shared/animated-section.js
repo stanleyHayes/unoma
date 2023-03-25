c063efa20e78b42eab12ba74ed6fb7f884c1dc01
Stanley Hayford <stanley@geometry.xyz>
2023-03-25 21:34:35 +0000

    Remove unnecessary imports

--- src/components/shared/animated-section.js
+++ src/components/shared/animated-section.js
@@ -1,4 +1,4 @@
-import {Box, Stack} from "@mui/material";
+import {Stack} from "@mui/material";
 import {motion} from "framer-motion";
 
 const AnimatedSection = ({children, backgroundColor = "background.default"}) => {
